7ea7909c619c66c91fc0ab4e8f4d60ffeadff71b
Ivana Djurkovic <ivanica0607@gmail.com>
2020-02-16 13:27:26 +0100

    Nedelja3

--- zadatak5/index.js
+++ zadatak5/index.js
@@ -1,23 +1,34 @@
-// ja sam domaci uradila pre petka tj. pre nego sto smo radili addEventListener,
-// ispravicu kod ali to ne stizem do predaje domaceg, nadam se da moze i ovako :)
 
-let button = document.getElementById("clickme"),
-  count = 0;
- function clickMe() {
-  count += 1;
-  button.innerHTML = "Order : " + count;
-};
+// let button = document.getElementById("clickme"),
+//   count = 0;
+//  function clickMe() {
+//   count += 1;
+//   button.innerHTML = "Order : " + count;
+// };
 
-function displayDate() {
-    if (count==0) {
-        document.getElementById("app1").innerHTML = `You didn't order`;
-    } else {
-        
+let pCount = document.querySelector('#clickMe');
+let counter = 0;
+
+let btnOrder = document.querySelector('#order');
+
+btnOrder.addEventListener('click', (e) => {
+  counter++
+  pCount.innerHTML = "Order : " + counter;
+})
+
+let btnYourOrder = document.querySelector('#app');
+let pYourOreder = document.querySelector('#app1');
+
+btnYourOrder.addEventListener('click', (e) => {
+  if (counter == 0) {
+    app1.innerHTML = `You didn't order`;
+  } else {
+
     let time = new Date();
-    document.getElementById("app1").innerHTML = `You ordered ${count} pizza <br> at 
+    app1.innerHTML = `You ordered ${counter} pizza <br> at 
      ${time.getHours()}: ${time.getMinutes()}: ${time.getSeconds()} 
-     on ${time.getDate()}. ${time.getMonth()+1}. ${time.getFullYear()}.`;
-    }
-}
+     on ${time.getDate()}. ${time.getMonth() + 1}. ${time.getFullYear()}.`;
+  }
+})
 
 
